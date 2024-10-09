ea5148276721fcac3340ffa16c1e5371efa9341a
lilmuzyvert <muhammad.muzamil@torontomu.ca>
2024-10-09 13:18:16 -0400

    edited

--- labfive.js
+++ labfive.js
@@ -2,12 +2,12 @@ function userName() {
     var name = prompt ("Please enter your name here");
 
     if (name) {
-        document.getElementById("welcomeMessage").textContent = 'Welcome. ' + name + '!';
+        document.getElementById("welcomeMessage").textContent = 'Welcome, ' + name + '!';
 } else {
     document.getElementsById('welcomeMessage').textContent = 'No Name Entered. Please enter your name.';
 }
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('nameButton').addEventListener('click', promptForName);
+    document.getElementById('nameButton').addEventListener('click', displayWelcomeMessage);
 });
