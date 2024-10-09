bb7af91c5155ad3330b54b9c8c1429918e4b64ca
lilmuzyvert <muhammad.muzamil@torontomu.ca>
2024-10-09 12:54:10 -0400

    Added Lab5

---
+++ Lab5.js
@@ -0,0 +1,13 @@
+function userName() {
+    var name = prompt ("Please enter your name here");
+
+    if (name) {
+        document.getElementById("welcomeMessage").textContent = 'Welcome. ' + name + '!';
+} else {
+    document.getElementsById('welcomeMessage').textContent = 'No Name Entered. Please enter your name.';
+}
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('nameButton').addEventListener('click', promptForName);
+});
