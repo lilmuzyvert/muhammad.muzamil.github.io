c6879593559aa65e8a95f33cb7ea8bcbd9349330
lilmuzyvert <muhammad.muzamil@torontomu.ca>
2024-10-30 19:38:14 -0400

    Added new seperate JS file.

--- Lab6New.js
+++ Lab6New.js
@@ -50,44 +50,5 @@ navbarLinks.forEach(link => {
     });
 });
 
-// New Code for Form Handling
 
-// Function to collect form data and store it in localStorage
-function submitForm() {
-    // Get form input values
-    const name = document.getElementById('name').value;
-    const email = document.getElementById('email').value;
-    const gender = document.querySelector('input[name="gender"]:checked').value;
-    const industry = document.getElementById('industry').value;
-
-    // Collect selected roles (multiple checkboxes)
-    const roles = [];
-    document.querySelectorAll('input[name="role"]:checked').forEach((checkbox) => {
-        roles.push(checkbox.value);
-    });
-
-    const comments = document.getElementById('comments').value;
-
-    // Create an object to store the form data
-    const formData = {
-        name: name,
-        email: email,
-        gender: gender,
-        industry: industry,
-        roles: roles,
-        comments: comments
-    };
-
-    // Store the object in localStorage
-    localStorage.setItem('contactFormData', JSON.stringify(formData));
-
-    // Notify the user that the form has been submitted
-    alert("Form Submitted! I will contact you soon!");
-}
-
-    // Function to clear localStorage if needed
-function clearStorage() {
-    localStorage.removeItem('contactFormData');
-    alert("Local storage cleared!");
-}
 
